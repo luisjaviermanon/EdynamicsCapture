f8ad4c65b204c47833da3e858cefa8c305e37f1e
luisjaviermanon <luisjavier261@gmail.com>
2024-06-01 17:24:16 -0400

    test

--- jest.setup.js
+++ jest.setup.js
@@ -17,3 +17,17 @@ jest.mock('react-native-vector-icons/MaterialCommunityIcons', () => {
   const MockIcon = props => React.createElement('MockIcon', props);
   return MockIcon;
 });
+jest.mock('react-native-vision-camera', () => {
+  return {
+    __esModule: true,
+    ...jest.requireActual('react-native-vision-camera'),
+    Camera: jest.fn().mockImplementation(() => {
+      return null;
+    }),
+    useCameraDevices: jest.fn().mockReturnValue({
+      devices: [],
+      selectedCamera: null,
+    }),
+    useFrameProcessor: jest.fn(),
+  };
+});
